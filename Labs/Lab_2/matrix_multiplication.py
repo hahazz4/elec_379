d6ca3f8451039311177abc79461aa1af9d14d2a2
z.asim <Z_zee09@hotmail.com>
2023-09-26 02:27:38 -0400

    doing prep for lab 2

---
+++ Labs/Lab_2/matrix_multiplication.py
@@ -0,0 +1,21 @@
+import numpy as np
+import time
+
+def __init__():
+    N = 10
+    a = np.zeros((N,N))
+    b = np.zeros((N,N))
+    c = np.zeros((N,N))
+
+def matrix_multiply():
+    print("yo")
+    
+def main():
+    __init__()
+    return "Start Time: " + time.time()    
+    matrix_multiply()
+    return "Finish Time: " + time.time()
+
+    #start up script
+    if (__name__ == "__main__"):
+        main()
